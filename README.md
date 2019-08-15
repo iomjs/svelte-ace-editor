07bd0339a205486bf31e0eafd24d10405d8ad39c
InstanceOfMichael <michael@codebro.org>
2019-08-15 19:11:36 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,86 @@
+svelte-ace-editor
+====================
+
+
+[![npm](https://img.shields.io/npm/v/svelte-ace-editor.svg)](https://www.npmjs.com/package/svelte-ace-editor)
+
+
+A packaging of [ace](https://ace.c9.io/)
+
+Demo here: https://github.com/chairuosen/vue-ace-editor-demo/tree/svelte
+
+## IMPORTANT
+emmet support for html is removed after 0.0.6. because its code cannot works with strict mode.
+
+if you want to use it. require emmet by your own.
+```
+npm install emmet@git+https://github.com/cloud9ide/emmet-core.git#41973fcc70392864c7a469cf5dcd875b88b93d4a
+```
+
+```js
+require(['emmet/emmet'],function (data) { // this is huge. so require it async is better
+    window.emmet = data.emmet;
+});
+```
+
+## How to use
+
+1. Install
+
+    ```
+    npm install --save-dev svelte-ace-editor
+    ```
+
+2. Use in a svelte component:
+
+    ```js
+    <div>
+        <AceEditor
+          bind:value={value}
+          theme="clouds_midnight"
+          lang={lang}
+          options={options}
+          width="100%"
+          height="512"
+          on:init={editorInit}
+          on:input={onEditorChange}
+        />
+    </div>
+
+    <script>
+      import AceEditor from 'svelte-ace-editor'
+
+      if (process.browser) {
+        require('brace');
+        // require('brace/ext/language_tools')
+        // require('brace/ext/language_tools')
+        // require('brace/mode/pgsql')
+        // require('brace/mode/mysql')
+        // require('brace/mode/sql')
+        // require('brace/mode/sqlserver')
+        require('brace/theme/clouds_midnight')
+      }
+
+      let value = ''
+      let lang = 'sql'
+      let options = {}
+
+      function editorInit (editor) {
+        // @todo something with editor or something after init
+      }
+      function onEditorChange (newValue) {
+        value = newValue.detail;
+      }
+
+    </script>
+    ```
+
+3. Further Notes:
+
+    prop `value`  is required
+
+    prop `lang` and `theme` is same as [ace-editor's doc](https://github.com/ajaxorg/ace)
+
+    prop `height` and `width` could be one of these:  `200`, `200px`, `50%`
+
+    this component is based on this component: https://github.com/chairuosen/vue2-ace-editor/tree/91051422b36482eaf94271f1a263afa4b998f099
---
+++ src/index.svelte
@@ -0,0 +1,106 @@
+<div
+  bind:this={element}
+  style="width:{width ? px(width) : '100%'};height:{height ? px(height) : '100%'}"
+></div>
+
+<script>
+  import { createEventDispatcher, tick, onMount, onDestroy } from 'svelte';
+
+  const ace = require('brace');
+  const dispatch = createEventDispatcher();
+  /**
+   * translation of vue component to svelte:
+   * @link https://github.com/chairuosen/vue2-ace-editor/blob/91051422b36482eaf94271f1a263afa4b998f099/index.js
+   **/
+  export let value; // String, required
+  export let lang; // String
+  export let theme; // String
+  export let height = null; // null for 100, else integer, used as percent
+  export let width = null; // null for 100, else integer, used as percent
+  export let options = null; // Object
+
+  let element = null; // bind this element to variable
+  let editor = null;
+  let contentBackup = '';
+
+  onDestroy(() => {
+    if (editor) {
+      editor.destroy();
+      editor.container.remove();
+    }
+  });
+
+  $: watchValue(value);
+  function watchValue (val) {
+    if (contentBackup !== val && editor && typeof val === 'string') {
+      editor.session.setValue(val,1);
+      contentBackup = val;
+    }
+  }
+
+  $: watchTheme(theme);
+  function watchTheme (newTheme) {
+    if (editor) {
+      editor.setTheme('ace/theme/'+newTheme);
+    }
+  }
+
+  $: watchMode(lang);
+  function watchMode (newOption) {
+    if (editor) {
+      editor.getSession().setMode('ace/mode/'+lang);
+    }
+  }
+
+  $: watchOptions(options);
+  function watchOptions (newOption) {
+    if (editor) {
+      editor.setOptions(newOption);
+    }
+  }
+
+  const resizeOnNextTick = () => tick().then(() => {
+    if (editor) {
+      editor.resize();
+    }
+  });
+
+  $: if (height!==null && width!==null) { resizeOnNextTick(); }
+
+  if (process.browser) {
+    onMount(() => {
+      lang = lang || 'text';
+      theme = theme || 'chrome';
+
+      require('brace/ext/emmet');
+
+      editor = ace.edit(element);
+
+      dispatch('init', editor);
+
+      editor.$blockScrolling = Infinity;
+      //editor.setOption("enableEmmet", true);
+      editor.getSession().setMode('ace/mode/'+lang);
+      editor.setTheme('ace/theme/'+theme);
+      editor.setValue(value, 1);
+      contentBackup = value;
+
+      editor.on('change',function () {
+        const content = editor.getValue();
+        dispatch('input', content);
+        contentBackup = content;
+      });
+      if (options) {
+        editor.setOptions(options);
+      }
+    });
+  }
+
+  const ValidPxDigitsRegEx = /^\d*$/;
+  function px (n) {
+    if (ValidPxDigitsRegEx.test(n) ){
+        return n+"px";
+    }
+    return n;
+  }
+</script>
